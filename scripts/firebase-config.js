24a4d87fa067c79d56d6e8b1b46c67794a61a813
oscherrero <36818091+oscherrero@users.noreply.github.com>
2025-04-02 18:23:03 +0200

    1

---
+++ scripts/firebase-config.js
@@ -0,0 +1,11 @@
+// Reemplaza con tus credenciales de Firebase
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQpcTpOIfzt-xDWt0wVsY-bAqJWrhPFh4",
+    authDomain: "avisosrcz.firebaseapp.com",
+    projectId: "avisosrcz",
+    storageBucket: "avisosrcz.firebasestorage.app",
+    messagingSenderId: "392353494797",
+    appId: "1:392353494797:web:9e73eacd62b4dbf5aba244",
+    measurementId: "G-3GHB3VK9GT"
+    };
